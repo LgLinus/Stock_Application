aa2b7598e3c24b14d6fb4358a559b5e1de525625
Linus Granath <linusgh@axis.com>
2018-11-23 22:19:01 +0100

    Added food endpoint

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -1,11 +1,12 @@
 function init(app) {
-  app.get('/food/getAll', async (req, res) => {
-    const foodData = [
-      {name: 'Spaghetti & Köttfärsås'},
-      {name: 'Tacos'},
-      {name: 'Kyckling med curry och ris'},
-    ];
-    res.send(foodData);
-  });
+  app.get('/food/getAll'),
+    async (req, res) => {
+      Recipe.find({title: 'Spaghetti'}, 'title ingredients', function(
+        err,
+        recipes,
+      ) {
+        res.send(recipes);
+      });
+    };
 }
 module.exports = init;
